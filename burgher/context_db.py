ab04f7a6ced246bfabf5c9e7dd6476b78adaeed3
martin <Visgean@gmail.com>
2024-10-29 23:23:04 +0100

    vlxk

--- burgher/context_db.py
+++ burgher/context_db.py
@@ -1,6 +1,5 @@
 import json
 import pathlib
-from textwrap import indent
 
 
 class ContextDB:
@@ -14,8 +13,6 @@ class ContextDB:
         self.keys_used = set()
 
     def get_key(self, key, hash):
-        self.keys_used.add(key)
-
         if not key or not hash:
             return None
 
@@ -23,11 +20,13 @@ class ContextDB:
         if not key_data:
             return None
 
+
         hash_old = key_data.get('hash')
         if hash_old != hash:
             self.data.pop(key)
             return None
 
+        self.keys_used.add(key)
         return key_data['data']
 
     def set_key(self, key, hash, data):
@@ -40,9 +39,9 @@ class ContextDB:
 
     def dump(self):
         # dump any unused keys:
-
         for key in list(self.data.keys()):
             if key not in self.keys_used:
+                print(f'Purging {key} data')
                 self.data.pop(key)
 
         with open(self.path, 'w') as f:
