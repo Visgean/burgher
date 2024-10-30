83089e0188b338b165727df37db4a343d283c760
martin <Visgean@gmail.com>
2024-10-30 15:39:07 +0100

    mode feed node to public package

--- burgher/__init__.py
+++ burgher/__init__.py
@@ -4,6 +4,7 @@ from .blog import BlogRoot
 from .gallery import Gallery
 from .node import Node
 from .static import StaticFolderNode, StaticNode
+from .feed import Feed
 from .template_nodes import (
     FileTemplateNode,
     FrontMatterNode,
